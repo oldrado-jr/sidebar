a8ed606ba6cf6aef9083bd171b8b7d0e5101adc3
Oldrado Junior <oldradojunior@gmail.com>
2023-10-26 06:50:32 -0300

    feat: add slide animation on the sidebar

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector('#menu-icon').addEventListener('click', () => {
-  document.querySelector('header').classList.toggle('open-sidebar');
+  document.querySelector('.sidebar').classList.toggle('open-sidebar');
 });
